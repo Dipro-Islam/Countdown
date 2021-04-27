83e3878960c35978887ebb5b7967a8105314d379
Toriqul Islam Dipro <78441463+Dipro-Islam@users.noreply.github.com>
2021-04-28 02:56:52 +0600

    Fix the Date
    1st commit

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ function countDown() {
     const ramadan = new Date(ramadanDate);
     const currentDate = new Date();
 
-    const diff = (ramadan - currentDate) / 1000;
+    // we get the answer in milisec , so devide by 1000 to get the sec
+    const difference = (ramadanDate - currentDate) / 1000;
 
-    var days = Math.floor(diff / 3600 / 24);
-    var hour = Math.floor(diff / 3600) % 24;
-    var min = Math.floor(diff / 60) % 60;
-    var sec = Math.floor(diff % 60);
+    var days = Math.floor(difference / 3600 / 24);
+    var hour = Math.floor(difference / 3600) % 24;
+    var min = Math.floor(difference / 60) % 60;
+    var sec = Math.floor(difference % 60);
 
 
 
